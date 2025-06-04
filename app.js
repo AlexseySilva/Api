e4bd3432e390de64512ee28b29a5cc4cace25b27
Alexsey <83961167+AlexseySilva@users.noreply.github.com>
2025-06-04 20:14:02 -0300

    aaa
    aaaa

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { dbUser, dbPassword, port} = require("./config/env");
+const erroHandler = require("./middlewares/errorMiddleware");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+
+// Cria uma instância do Express
+const app = express();
+
+// Configura o express para entender req. em Json
+app.use(express.json());
+
+// Rota aberta
+app.get("/", (requisicao, resposta) => {
+  resposta.status(200).send({ msg: "Bem vindo a API!" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(erroHandler);
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(
+<<<<<<< HEAD
+      `mongodb+srv://${dbUser}:${dbPassword}@api.zubui.mongodb.net/?retryWrites=true&w=majority&appName=api`
+=======
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.cp2pd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+>>>>>>> 71eb3ba80be6ee57294063616df78d579b2d9cc2
+    );
+    console.log("Conectou ao banco MongoDB")
+
+    app.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
+  } catch (err) {
+    console.error("Erro ao conectar ao MongoDB", err);
+    process.exit(1);
+  }
+};
+
+// Inicia o servidor apenas se Não estiver em ambiente de teste.
+if (process.env.NODE_ENV !== "test") {
+  startServer();
+}
+
+module.exports = app
+
+// Inicia o servidor na porta 3000
+// mongoose
+//   .connect(`mongodb+srv://${dbUser}:${dbPassword}@api.isusp.mongodb.net/?retryWrites=true&w=majority&appName=API`)
+//   .then(() =>{
+//     console.log("Conectado ao MongoDB");
+//     app.listen(port, () => {
+//       console.log(`Servidor rodando na porta ${port}`);
+//     });
+// })
+// .catch((err) => {
+//   console.error("Erro ao conectar ao MongoDB", err);
+//   process.exit(1);
+// });
